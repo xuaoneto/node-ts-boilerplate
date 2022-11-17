1dba5fa735818e121c38917d98792255748e690b
João Fernandes Praxedes Neto <jofernandes47@gmail.com>
2022-11-17 00:00:12 -0300

    Delete dist directory

--- dist/utils/mongo-errors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongoErrors = void 0;
-exports.mongoErrors = {
-    112: "Conflito",
-    11000: "Cadastro Duplicado",
-    211: "Banco de dados caiu ou tem um erro de configuração",
-    11600: "Banco de dados caiu ou tem um erro de configuração",
-};
